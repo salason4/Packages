4110f19140397ec5ffe83d431c8f24ae79712b3b
Engr Horshy <salawudeensodiq8@gmail.com>
2023-02-04 23:17:10 +0100

    Initial commit

---
+++ src/Pakages/Main.java
@@ -0,0 +1,10 @@
+package Pakages;
+
+import pakageTools.Toolbox;
+
+public class Main {
+    public static void main(String[] args) {
+        Toolbox tool =new Toolbox();
+    }
+
+}
---
+++ src/pakageTools/Toolbox.java
@@ -0,0 +1,5 @@
+package pakageTools;
+
+public class Toolbox {
+
+}
